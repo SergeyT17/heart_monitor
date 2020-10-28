6a5fca5b81d8bcb06f2d8af38c2c4c26a77ebca1
Sergey <seryoga1796@gmail.com>
2020-10-28 19:17:13 +0300

    update gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,33 +1,81 @@
-const gulp        = require('gulp');
-const browserSync = require('browser-sync');
-const sass        = require('gulp-sass');
-const cleanCSS = require('gulp-clean-css');
+const {src, dest, parallel, series, watch} = require('gulp');
+const browserSync  = require('browser-sync').create();
+const sass         = require('gulp-sass');
+const cleancss     = require('gulp-clean-css');
 const autoprefixer = require('gulp-autoprefixer');
-const rename = require("gulp-rename");
+const rename       = require('gulp-rename');
+const concat       = require('gulp-concat');
+const imagemin     = require('gulp-imagemin');
+const newer        = require('gulp-newer');
+const del          = require('del');
+const uglify       = require('gulp-uglify-es').default;
 
-gulp.task('server', function() {
+function browsersync() {
+    browserSync.init({
+        server: { baseDir: "src/" },
+        notify: false,
+    })
+}
 
-    browserSync({
-        server: {
-            baseDir: "src"
-        }
-    });
+// function scripts() {
+//     return src([
+//         'node_modules/jquery/dist.jquery.min.js'
+//         'src/js/app.js',
+//     ])
+//     .pipe(concat('app.min.js'))
+//     .pipe(uglify())
+//     .pipe(dest('src/js/')
+//     .pipe(browserSync.stream())
+// }
 
-    gulp.watch("src/*.html").on('change', browserSync.reload);
-});
+function styles() {
+    return src('src/scss/style.scss')
+    .pipe(sass())
+    .pipe(concat('style.min.css'))
+    .pipe(autoprefixer({ overrideBrowserslist: ['last 7 versions'], grid: true }))
+    .pipe(cleancss(({ level:{ 1: {specialComments: 0 }}})))
+    .pipe(dest('src/css/'))
+    .pipe(browserSync.stream())
+}
 
-gulp.task('styles', function() {
-    return gulp.src("src/sass/**/*.+(scss|sass)")
-        .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-        .pipe(rename({suffix: '.min', prefix: ''}))
-        .pipe(autoprefixer())
-        .pipe(cleanCSS({compatibility: 'ie8'}))
-        .pipe(gulp.dest("src/css"))
-        .pipe(browserSync.stream());
-});
+function images() {
+    return src('src/img/original/**/*')
+    .pipe(newer('src/img/optimized/'))
+    .pipe(imagemin())
+    .pipe(dest('src/img/optimized/'))
+}
 
-gulp.task('watch', function() {
-    gulp.watch("src/sass/**/*.+(scss|sass)", gulp.parallel('styles'));
-})
+function cleanimg() {
+    return del('src/img/optimized/**/*', {force: true})
+}
 
-gulp.task('default', gulp.parallel('watch', 'server', 'styles'));
+function startwatch() {
+    watch('src/**/*.scss', styles);
+    watch('src/**/*.html').on('change', browserSync.reload);
+    watch('src/img/original/**/*', images);
+    // watch(['src/**/*.js', '!src/**/*.min.js'], scripts)
+}
+
+function buildcopy() {
+    return src([
+        'src/css/**/*.min.css',
+        'src/image/optimized/**/*',
+        'src/fonts/*',
+        'src/icons/*.png',
+        'src/**/*.html'
+         // 'src/js/**/*.min.js',
+    ], { base: 'src'})
+    .pipe(dest('dist'))
+}
+
+// exports.scripts  = scripts;
+exports.browsersync = browsersync;
+exports.styles      = styles;
+exports.images      = images;
+exports.cleanimg    = cleanimg;
+exports.buildcopy   = series(styles, images, buildcopy);
+
+exports.default     = parallel( styles, browsersync, startwatch); 
+//когда подключишь js сюда не забудь добавить
+
+
